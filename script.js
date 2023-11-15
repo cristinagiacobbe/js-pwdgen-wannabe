64da625d08d0cf40aadaba2cd0104c4bf272bd27
cristinagiacobbe <crigiak@gmail.com>
2023-11-15 22:29:45 +0100

    Initialize html and js files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/* Chiedi all'utente il suo nome
+poi chiedi all'utente il suo cognome
+poi chiedi all'utente il suo colore preferito
+scrivi sulla pagina nomecognomecolorepreferito23
+Esempio: cristinagiacobbepesca23 */
+
+//variable firstname
+//variable lastname
+//variable favourite_color
+//prompt firstname
+//prompt lastname
+//prompt favourite_color
+//document
+//getElementById
+//innerHTML
+
